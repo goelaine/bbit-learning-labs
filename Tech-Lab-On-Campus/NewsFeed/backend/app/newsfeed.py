34225c2ec91fd520e209b7430eac71115f8621eb
Elaine Gombos <147881417+goelaine@users.noreply.github.com>
2025-04-09 23:37:50 +0000

    backend part 1

--- Tech-Lab-On-Campus/NewsFeed/backend/app/newsfeed.py
+++ Tech-Lab-On-Campus/NewsFeed/backend/app/newsfeed.py
@@ -15,14 +15,28 @@ class Article:
     image_url: str
     url: str
 
+def format_article(js) -> Article:
+    art_dict = json.loads(js)
+    image_url = art_dict["main_image"]
+    author = art_dict['author']
+    title = art_dict["title"]
+    body = art_dict["text"]
+    publish_date = art_dict["published"]
+    url = art_dict["url"]
+
+
+    return Article(author, title, body, publish_date, image_url, url)
 
 def get_all_news() -> list[Article]:
     """Get all news articles from the datastore."""
     # 1. Use Redis client to fetch all articles
     arts = REDIS_CLIENT.get_entry("all_articles")
     # 2. Format the data into articles
+    acc = []
+    for js in arts:
+        acc.append(format_article(js))
     # 3. Return a list of the articles formatted 
-    return []
+    return acc
 
 
 def get_featured_news() -> Article | None:
